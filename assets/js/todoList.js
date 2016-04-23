19bb0fddb54ee849940a00cab9c066cf95d36b19
bklynate <nathanielcfa@gmail.com>
2016-04-23 10:14:58 -0400

    edit completed feature logic

--- assets/js/todoList.js
+++ assets/js/todoList.js
@@ -1,18 +1,4 @@
 // check off specific todos by clicking
 $("li").on("click",function(){
-  // if li if gray
-  if(this.style.color === "gray"){
-  // turn it black & no line-through
-    $(this).css({
-      color:"black",
-      textDecoration: "none"
-    });
-  } else {
-  // turn it gray
-    $(this).css({
-      color: "gray",
-      textDecoration: "line-through",
-    });
-  }
-  // $(this).toggleClass("completed")
+  $(this).toggleClass("completed")
 });
