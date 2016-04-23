ae55b23b7fa4fcf1c8ec083cdba6c9616edc16a7
bklynate <nathanielcfa@gmail.com>
2016-04-23 11:26:32 -0400

    add delete feature for items in todo list, capturing user input logic into a variable

--- assets/js/todoList.js
+++ assets/js/todoList.js
@@ -2,3 +2,21 @@
 $("li").on("click",function(){
   $(this).toggleClass("completed")
 });
+
+// click on x to delete todo
+$("span").on("click", function(event){
+  $(this).parent().fadeOut(500,function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+// takes text input and creates new todo items
+$("input[type='text']").on("keypress", function(event){
+  // listens for the keypress of 13
+  // which is the value the enter key
+  if(event.which === 13){
+    // puts value of user input into todoItem
+    var todoItem = $(this).val();
+  }
+})
